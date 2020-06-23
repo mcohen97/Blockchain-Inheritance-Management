9dbe8a8ed3af7348b74d6624d44cfd21e72d2a03
= <diegomarcel27@hotmail.com>
2020-06-23 18:28:54 -0300

    Added suscription and unsuscription of managers.

--- contracts/Testament.sol
+++ contracts/Testament.sol
@@ -3,28 +3,36 @@ pragma solidity ^0.5.1;
 contract Testament {
 
     address payable public owner;
-    address payable[] heirs;
-    uint8[] percentages;
-    address payable[] managers;
+    address payable[] public heirs;
+    uint8[] public percentages;
 
-    constructor(address payable[] memory _heirs, uint8[] memory _cutPercents, address payable[] memory _managers) public payable{
+    address payable[] public managers;
+
+    constructor(address payable[] memory _heirs, uint8[] memory _cutPercents,
+                address payable[] memory _managers) public payable{
+
         require(_heirs.length > 0, "The testament must have at least one heir.");
         require(_heirs.length == _cutPercents.length, "Heirs' addresses and cut percentajes counts must be equal.");
         require(managersWithinBounds(_managers.length), "There can only be between 2 and 5 managers");
         require(addUpTo100(_cutPercents), "Percentajes must add up to 100");
 
         owner = msg.sender;
-        managers = _managers;
-        heirs = _heirs;
+        setHeirs(_heirs);
+        setManagers(_managers);
         percentages = _cutPercents;
     }
 
-    function destroy() public onlyOwner {
-        selfdestruct(owner);
-    }
 
     function managersWithinBounds(uint count) private pure returns (bool) {
-        return count >= 2 && count <= 5;
+        return aboveManagersLowerLimit(count) && belowManagersUpperLimit(count);
+    }
+
+    function belowManagersUpperLimit(uint count) private pure returns (bool) {
+        return count <= 5;
+    }
+
+    function aboveManagersLowerLimit(uint count) private pure returns (bool) {
+        return count >= 2;
     }
 
     function addUpTo100(uint8[] memory percents) private pure returns (bool) {
@@ -33,6 +41,37 @@ contract Testament {
             sum += percents[i];
         }
         return sum == 100;
+    }
+
+    function setHeirs(address payable[] memory _heirs) private{
+        heirs = _heirs;
+    }
+
+    function setManagers(address payable[] memory _managers) private {
+        managers = _managers;
+    }
+
+    function suscribeManager(address payable newManager) public onlyOwner{
+        require(belowManagersUpperLimit(managers.length + 1), "Managers maximum exceeded");
+        managers.push(newManager);
+    }
+
+    function unsuscribeManager(address payable toDelete) public onlyOwner {
+        require(aboveManagersLowerLimit(managers.length - 1), "Managers minimum not reached");
+        for(uint8 i = 0; i < managers.length; i++) {
+            if(toDelete == managers[i]){
+                delete managers[i];
+                i++;
+                for(; i < managers.length; i++){
+                    managers[i-1] = managers[i];
+                }
+            }
+        }
+        delete managers[managers.length - 1];
+    }
+
+    function destroy() public onlyOwner {
+        selfdestruct(owner);
     }
 
     modifier onlyOwner(){
