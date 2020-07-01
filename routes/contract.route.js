75b9be242ad30fcc393035a00544377e9bfa5aac
= <diegomarcel27@hotmail.com>
2020-06-30 23:45:19 -0300

    Added endpoints for adding a new heir and geting heir by priority.

--- routes/contract.route.js
+++ routes/contract.route.js
@@ -102,5 +102,45 @@ router.get("/managers/:pos", async function(req, res){
 });
 
 
+router.post("/heirs", async function(req, res){
+  try{
+    const executor = req.body.from;
+    const address = req.body.heir;
+    const percent = req.body.percentage;
+    const priority = req.body.priority;
+    
+    const contract = contractService.getContract();
+
+    let result = await contract.methods.suscribeHeir(address, percent, priority)
+      .send({
+          from: executor,
+          gas: 1000000
+      });
+
+    res.status(200).send('Ok');
+
+  }catch(error){
+    res.send(500).send(`Cannot execute method: ${error.message}`);
+  }
+});
+
+router.get("/heirs/:pos", async function(req, res){
+  
+  try{
+    const manager = req.params.pos;
+    const contract = contractService.getContract();
+
+    let result = {}
+    result['heir'] = await contract.methods.heirs(manager).call();
+    result['percentage'] = await contract.methods.percentages(manager).call();
+
+    res.status(200).send(result);
+
+  }catch(error){
+    res.send(500).send(`Cannot execute method: ${error.message}`);
+  }
+});
+
+
 
 module.exports = router;
