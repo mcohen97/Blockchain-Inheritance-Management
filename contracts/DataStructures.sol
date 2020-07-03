86a374e7f068ca65a269f4bd19582e09bb5559e7
= <diegomarcel27@hotmail.com>
2020-07-03 00:39:21 -0300

    Added data structures contract.

---
+++ contracts/DataStructures.sol
@@ -0,0 +1,18 @@
+pragma solidity ^0.5.1;
+
+contract DataStructures {
+    struct OwnerData {
+        string fullName;
+        string id;
+        uint256 birthdate;
+        string homeAddress;
+        string telephone;
+        string email;
+        uint256 issueDate;
+    }
+
+    struct HeirData {
+        address payable heir;
+        uint8 percentaje;
+    }
+}
