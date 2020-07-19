32d899cd7b85939183588822cb1094c1db8b785f
AlanMonjardin <alanmonjardin@outlook.com>
2020-07-19 20:33:59 -0300

    Added charitable org. to law contract constructor.

--- routes/laws_contract_route.js
+++ routes/laws_contract_route.js
@@ -15,7 +15,7 @@ router.post("/compile", function (req, res) {
 router.post("/deploy", function (req, res) {
   try {
     const body = req.body;
-    contractService.deployLaws(body.from);
+    contractService.deployLaws(body.from, body.charitableOrganization);
     res.status(200).send('OK');
   } catch (error) {
     res.status(500).send(new Error('Cannot deploy contract'));
